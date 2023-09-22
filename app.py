4e9a5a065307b7ff076067cb875a154e2612bc66
Юрий Комаров <yurakom06@gmail.com>
2023-09-22 12:46:34 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,32 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route("/index")
 def start():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <ol>
+        <li><a href="http://127.0.0.1:5000/lab1" target="_blank">Первая лабораторная</a></li>
+        </ol>
+
+        <footer>
+            &copy; ФИО, группа, курс, год
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!doctype html>
 <html>
