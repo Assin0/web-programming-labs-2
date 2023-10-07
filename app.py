9290fe3a7cf30901c1459d4bc8160705afd701cb
Юрий Комаров <yurakom06@gmail.com>
2023-10-07 07:35:51 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ def example():
     number="2"
     group="ФБИ-11"
     course="3 курс"
-    return render_template("example.html", name=name, number=number, group=group, course=course)
+    fruits = [
+        {'name':'яблоки', 'price': 100},
+        {'name':'груши', 'price': 120},
+        {'name':'апельсины', 'price': 80},
+        {'name':'мандарины', 'price': 95},
+        {'name':'манго', 'price': 321}
+        ]
+    return render_template("example.html", name=name, number=number, group=group, course=course, fruits=fruits)
 
 @app.route('/')
 @app.route("/index")
