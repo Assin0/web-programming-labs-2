1fbd232c32ba2bea58e969f22846d2e17c47c5b1
Юрий Комаров <yurakom06@gmail.com>
2023-11-18 03:58:59 +0700

    Заготовка к 4-ой лабораторной

--- lab1.py
+++ lab1.py
@@ -25,6 +25,7 @@ def menu():
         <li><a href="/lab1" target="_blank">Первая лабораторная</a></li>
         <li><a href="/lab2" target="_blank">Вторая лабораторная</a></li>
         <li><a href="/lab3" target="_blank">Третья лабораторная</a></li>
+        <li><a href="/lab4" target="_blank">Четвертая лабораторная</a></li>
         </ol>
 
         <h2>Реализованные роуты</h2>
