48760fb7f95a0663910577ad59d00e9752bcf708
Юрий Комаров <yurakom06@gmail.com>
2023-11-18 06:04:46 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -5,3 +5,24 @@ lab4 = Blueprint('lab4',__name__)
 @lab4.route("/lab4/")
 def lab():
     return render_template('lab4.html')
+
+@lab4.route("/lab4/login", methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == 'alex' and password == '123':
+        return render_template('success.html', username=username)
+    elif username == '' or password == '':
+        error = 'Не введён логин или не введён пароль'
+        username = username
+        password = password
+        return render_template('login.html', error=error, username=username, password=password)
+    else:
+        error = 'Неверный логин и/или пароль'
+        username = username
+        password = password
+        return render_template('login.html', error=error, username=username, password=password)
+    
