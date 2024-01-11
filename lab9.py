08bc27ff366fce058f1ff3f40b9f9649d8875d3a
Юрий Комаров <yurakom06@gmail.com>
2024-01-11 20:41:55 +0700

    Конец 9 лабораторной

--- lab9.py
+++ lab9.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, session
+import psycopg2
 
 lab9 = Blueprint('lab9', __name__)
 
@@ -18,3 +19,17 @@ def error():
 @lab9.app_errorhandler(500)
 def error500(e):
     return render_template('lab9/error500.html')
+
+@lab9.route('/lab9/otkritka', methods=['GET', 'POST'])
+def otkritka():
+    otkritka = ""
+    username = request.form.get('username')
+    username2 = request.form.get('username2')
+    sex = request.form.get('sex')
+
+    if username and username2 and sex == 'м':
+        otkritka = f'Желаю счастья, чтобы  ты {username2} был самым здоровым человеком и в новом году ты получил все что желаешь! P. S. {username}'
+
+    if username and username2 and sex == 'ж':
+        otkritka = f'Желаю счастья, чтобы  ты {username2} была самым здоровым человеком и в новом году ты получила все что желаешь! P. S. {username}'
+    return render_template('lab9/otkritka.html', otkritka=otkritka)
