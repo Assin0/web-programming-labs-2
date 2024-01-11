a0c7d740e1c226b2428da1c5bf443b11f74363e4
Юрий Комаров <yurakom06@gmail.com>
2024-01-11 18:19:31 +0700

    Промежуточное сохранение по 9 лабораторной

--- lab8.py
+++ lab8.py
@@ -6,3 +6,14 @@ lab8 = Blueprint('lab8', __name__)
 @lab8.route('/lab8/')
 def main():
     return render_template('lab8/index.html')
+
+courses = [
+    {"name": "c++", "videos": 3, "price": 3000},
+    {"name": "basic", "videos": 30, "price": 0},
+    {"name": "c#", "videos": 8},
+]
+
+
+@lab8.route('/lab8/api/courses/', methods=['GET'])
+def get_courses():
+    return courses
--- lab9.py
+++ lab9.py
@@ -8,4 +8,13 @@ def main():
 
 @lab9.app_errorhandler(404)
 def not_found(e):
-    return "нет такой страницы", 404
+    return render_template('lab9/error404.html')
+
+@lab9.route('/lab9/500')
+def error():
+    result = 7 / 0
+    return result
+
+@lab9.app_errorhandler(500)
+def error500(e):
+    return render_template('lab9/error500.html')
